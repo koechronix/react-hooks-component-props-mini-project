4610404f0a73e1dd7a34deca52a82691e193c2fc
koechronix <koechronix@yahoo.com>
2022-07-17 20:35:07 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-      <Header name={blogData.image}/>
+      <Header name={blogData.name}/>
       <About image={blogData.image} about={blogData.about}/>
       <ArticleList posts={blogData.posts}/>
 
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Article from "./Article"
+import Article from "./Article";
 
 function ArticleList({posts}) {
     const articles = posts.map((post)=>(
